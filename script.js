7bddb26d3ba0caafe8f01d2bb736a8b87600517c
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-09-30 21:28:30 +0300

    добавлены переменные в js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+let title = 'Title';
+let screens = 'screens';
+let screenPrice = 'screenPrice';
+let rollback = 'rollback';
+let fullPrice = 'fullPrice';
+let adaptive = 'adaptive';
+
 alert("Hello, Alert");
 console.log("Hello, console");
