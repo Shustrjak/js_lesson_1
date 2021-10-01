8412d415621b39b47e32635245cf9adf470b565f
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-01 21:29:08 +0300

    создание калькулятора расчетов стоимости разработки

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let someText = 'Hello, friend!';
+let title = 'Калькулятор разработки';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 100;
+let rollback = 50;
+let fullPrice = 10000;
+let adaptive = false;
 
-alert("Hello, Alert");
-console.log("Hello, console");
+
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани.');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/долларов/гривен/юани.');
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice*(rollback/100));
