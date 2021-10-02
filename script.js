dac7889ff63608c1a23cc31e08178b4a6f488269
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-02 20:01:45 +0300

    refactor script.js

--- script.js
+++ script.js
@@ -11,4 +11,4 @@ console.log(screens.length);
 console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани.');
 console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/долларов/гривен/юани.');
 console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice*(rollback/100));
+console.log(fullPrice * (rollback / 100));
