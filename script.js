5eb8c948384d952be577bc05c414cc5c1dc20b3f
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-04 22:01:08 +0300

    homework 3rd day

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const title = 'Калькулятор разработки';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 100;
-const rollback = 50;
-const fullPrice = 10000;
-const adaptive = false;
+'use strict';
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "пример: Простые, Сложные, Интерактивные");
+const screenPrice = parseInt(prompt("Сколько будет стоить данная работа?", "12000"));
+const rollback = 36;
+const adaptive = confirm("Нужен ли адаптив на сайте?"); 
+const service1 = prompt('Какой дополнительный тип услуг нужен?');
+const servicePrice1 = parseInt(prompt('Сколько будет стоить?'));
+const service2 = prompt('Какой дополнительный тип услуг нужен?');
+const servicePrice2 = parseInt(prompt('Сколько будет стоить?'));
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = fullPrice / 100 * rollback;
+
+// вариант 1 вывод в консоль
+switch (true) {
+    case fullPrice >= 30000:
+        console.log('Даем скидку 10%');        
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log('Даем скидку 5%');        
+        break;    
+    case fullPrice > 0 && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');    
+        break;
+    default:
+        console.log('Что то пошло не так');
+        break;
+}
+
+// вариант 2 вывод в консоль
+
+
+if ( fullPrice >= 30000 ) {
+    console.log('Даем скидку 10%');
+} else if ( fullPrice >= 15000 && fullPrice < 30000 ){
+    console.log('Даем скидку 5%');
+} else if ( fullPrice >= 0 && fullPrice < 15000 ){
+    console.log('Скидка не предусмотрена');
+} else {
+    console.log('Что то пошло не так');
+}
 
 
 console.log(title, fullPrice, adaptive);
@@ -11,4 +46,5 @@ console.log(screens.length);
 console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани.');
 console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/долларов/гривен/юани.');
 console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
+console.log( fullPrice - Math.ceil(fullPrice * (rollback / 100)));
+
